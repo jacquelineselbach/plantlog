ffe1c60736fe8487e57867a010d3191f458cf4fe
Jacqueline <jacqueline.selbach@stud.fh-campuswien.ac.at>
2025-12-18 21:38:36 +0100

    Initial Commit

---
+++ plantlog/plantlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  plantlogApp.swift
+//  plantlog
+//
+//  Created by Jacqueline Selbach on 18.12.25.
+//
+
+import SwiftUI
+
+@main
+struct plantlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ plantlogTests/plantlogTests.swift
@@ -0,0 +1,17 @@
+//
+//  plantlogTests.swift
+//  plantlogTests
+//
+//  Created by Jacqueline Selbach on 18.12.25.
+//
+
+import Testing
+@testable import plantlog
+
+struct plantlogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
